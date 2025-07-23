f1c7c525cde979598c569a1e99f26578e31e389a
KelvinMbugii <wanjuguKelvin851@gmail.com>
2025-07-24 00:00:01 +0300

    updated allowheaders in cors to accept array

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.use(
     },
     credentials: true,
     methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: ['Content-Type, Authorization'],
    
   })
 );
