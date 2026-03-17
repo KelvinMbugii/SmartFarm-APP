87a7931ac3df2b561f504a32cc440e6636a68eb2
Mugorufas <mugorufus072@gmail.com>
2026-03-17 14:05:55 +0300

    added patch function in CORS

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
